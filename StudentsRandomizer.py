e683dff31fc0496cc354123c4981acde123c8a99
Marcos-A <marcosalcocer@gmail.com>
2020-12-14 12:09:41 +0100

    Modified .gitignore file. Added txt file export of the result.

--- StudentsRandomizer.py
+++ StudentsRandomizer.py
@@ -4,10 +4,12 @@
 import pandas as pd
 import random
 
-FILENAME = "students.csv"
+SOURCE_FILENAME = "students.csv"
+RESULT_FILENAME = "randomly_sorted_students.txt"
+
 
 def get_students_names_to_list():
-    students_file = pd.read_csv(FILENAME)
+    students_file = pd.read_csv(SOURCE_FILENAME)
     return students_file['NAME'].tolist()
 
 
@@ -15,14 +17,26 @@ def randomize_students_list(students_list):
     return random.sample(students_list, len(students_list))
 
 
-def print_numbered_list_elements(students_list):
+def export_randomly_sorted_students_to_txt(students_list):
+    txt_file = open(RESULT_FILENAME, "w")
     i = 1
     for student in students_list:
-        print(str(i) + ". " + student)
+        numbered_randomly_sorted_student = str(i) + ". " + student
+        txt_file.write(numbered_randomly_sorted_student + "\n")
         i = i+1
+    txt_file.close()
+
+
+def print_randomly_sorted_students():
+    txt_file = open(RESULT_FILENAME, "r")
+    txt_file_lines = txt_file.read().splitlines()
+    for line in txt_file_lines:
+        print(line)
+    txt_file.close()
 
 
 if __name__ == "__main__":
     students_list = get_students_names_to_list()
     randomized_students_list = randomize_students_list(students_list)
-    print_numbered_list_elements(randomized_students_list)
+    export_randomly_sorted_students_to_txt(randomized_students_list)
+    print_randomly_sorted_students()
